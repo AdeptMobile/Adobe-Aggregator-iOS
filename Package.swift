466bc32e6832a85eff06fb77b44b3682dfd0089f
john Brunelle <anon4949@gmail.com>
2025-01-28 14:08:16 -0500

    tweak

--- Package.swift
+++ Package.swift
@@ -1,17 +1,36 @@
-// swift-tools-version:5.9
-//.package(url: "https://github.com/adobe/aepsdk-assurance-ios.git", exact: "5.0.1"),
+
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Adode_AEP_SDK",
+    name: "igi_sdk",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "adobe_sdk",
+            targets: ["abobe_sdk_source"]),
+    ],
     dependencies: [
+        .package(url: "https://github.com/adobe/aepsdk-assurance-ios.git", exact: "5.0.1"),
         .package(url: "https://github.com/adobe/aepsdk-core-ios.git", exact: "5.3.2")
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Adode_AEP_SDK", 
-            dependencies: [.product(name: "AEPCore", package: "aepsdk-core-ios")], 
-            path: "Sources/mylib"
-        ),
+            name: "abobe_sdk_source",
+            dependencies: [
+                //.target(name: "abobe_sdk", condition: .when(platforms: [.iOS])),
+                .product(name: "AEPAssurance", package: "aepsdk-assurance-ios"),
+                .product(name: "AEPCore", package: "aepsdk-core-ios")
+            ]
+            //path: "Sources"
+        )
+        //.binaryTarget(name: "adobe_sdk", path: "./Sources/Frameworks/igi_sdk.xcframework")
     ]
 )
---
+++ Sources/adobe_sdk/dummy.md
@@ -0,0 +1,13 @@
+//
+//  dummy.h
+//  
+//
+//  Created by Hammad Mehmood on 25/01/2024.
+//
+
+#ifndef dummy_h
+#define dummy_h
+
+//#include <igi_sdk.h>
+
+#endif /* dummy_h */
