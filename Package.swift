b4cc18c7d52f1cad69d054c7d8f8e6875c14c3e7
john Brunelle <anon4949@gmail.com>
2025-01-28 11:15:32 -0500

    tweak

--- Package.swift
+++ Package.swift
@@ -8,26 +8,8 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "AEP_SDK",
-            targets: ["AEP_SDK"]),
-    ],
     dependencies: [
         .package(url: "https://github.com/adobe/aepsdk-core-ios.git", exact: "5.3.2"),
         .package(url: "https://github.com/adobe/aepsdk-assurance-ios.git", exact: "5.0.1"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AEP_SDK",
-            dependencies: [
-                .target(name: "AEP_SDK", condition: .when(platforms: [.iOS])),
-                .product(name: "AEPCore", package: "aepsdk-core-ios"),
-                .product(name: "AEPAssurance", package: "aepsdk-assurance-ios")
-            ]
-        )
     ]
 )
