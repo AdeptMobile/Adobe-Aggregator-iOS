d1aa74e94ae59bf64cafb3dbae5b9651ae3c0efc
john Brunelle <anon4949@gmail.com>
2025-01-28 11:31:32 -0500

    tweak

--- Package.swift
+++ Package.swift
@@ -1,15 +1,28 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "AEP_SDK",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15), // Specify supported platforms (optional)
+    ],
+    products: [
+        // Define the executables and libraries your package produces.
+        .library(
+            name: "AEP_SDK",
+            targets: ["AEP_SDK"]),
     ],
     dependencies: [
         .package(url: "https://github.com/adobe/aepsdk-core-ios.git", exact: "5.3.2"),
         .package(url: "https://github.com/adobe/aepsdk-assurance-ios.git", exact: "5.0.1"),
+    ],
+    targets: [
+        // Define your targets (modules).
+        .target(
+            name: "AEP_SDK",
+            dependencies: [
+                .product(name: "AEPCore", package: "aepsdk-core-ios"),
+                .product(name: "AEPAssurance", package: "aepsdk-assurance-ios")
+            ])
     ]
 )
