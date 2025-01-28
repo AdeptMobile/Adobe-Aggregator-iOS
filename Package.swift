50b62527d17c5242f8eac1145a6ad06e2fc4ce37
john Brunelle <anon4949@gmail.com>
2025-01-28 17:02:10 -0500

    tweak

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -7,23 +7,34 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DependencyAggregator",
             targets: ["DependencyAggregator"]),
     ],
     dependencies: [
-        // Add your dependencies here
         .package(url: "https://github.com/adobe/aepsdk-assurance-ios.git", exact: "5.0.1"),
         .package(url: "https://github.com/adobe/aepsdk-core-ios.git", exact: "5.3.2")
+        .package(url: "https://github.com/adobe/aepsdk-edge-ios.git", exact: "5.0.3"),
+        .package(url: "https://github.com/adobe/aepsdk-edgeconsent-ios.git", exact: "5.0.0"),
+        .package(url: "https://github.com/adobe/aepsdk-edgeidentity-ios.git", exact: "5.0.0"),
+        .package(url: "https://github.com/adobe/aepsdk-messaging-ios.git", exact: "5.6.0"),
+        .package(url: "https://github.com/adobe/aepsdk-optimize-ios", exact: "5.2.0"),
+        .package(url: "https://github.com/adobe/aepsdk-places-ios", exact: "5.0.0"),
+        .package(url: "https://github.com/adobe/aepsdk-userprofile-ios.git", exact: "5.0.0")
     ],
     targets: [
-        // Add an empty target (optional, to resolve dependencies)
         .target(
             name: "DependencyAggregator",
             dependencies: [
                 .product(name: "AEPAssurance", package: "aepsdk-assurance-ios"),
-                .product(name: "AEPCore", package: "aepsdk-core-ios")
+                .product(name: "AEPCore", package: "aepsdk-core-ios"),
+                .product(name: "AEPEdge", package: "aepsdk-edge-ios"),
+                .product(name: "AEPEdgeConsent", package: "aepsdk-edgeconsent-ios"),
+                .product(name: "AEPEdgeIdentity", package: "aepsdk-edgeidentity-ios"),
+                .product(name: "AEPMessaging", package: "aepsdk-messaging-ios"),
+                .product(name: "AEPOptimize", package: "aepsdk-optimize-ios"),
+                .product(name: "AEPPlaces", package: "aepsdk-places-ios"),
+                .product(name: "AEPUserProfile", package: "aepsdk-userprofile-ios")
             ]
         )
     ]
